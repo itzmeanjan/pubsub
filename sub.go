289c0a27b9dfcd19423dfea519acaf8cef433b24
Anjan Roy <anjanroy@yandex.com>
2021-04-27 20:10:13 +0530

    read from pub/sub

--- sub.go
+++ sub.go
@@ -1,5 +1,7 @@
 package pubsub
 
+import "time"
+
 type PublishedMessage struct {
 	Topic string
 	Data  []byte
@@ -20,6 +22,23 @@ type Subscriber struct {
 	Id      uint64
 	Channel chan *PublishedMessage
 	Topics  map[string]bool
+}
+
+func (s *Subscriber) Next() *PublishedMessage {
+	if len(s.Channel) != 0 {
+		return <-s.Channel
+	}
+
+	return nil
+}
+
+func (s *Subscriber) BNext(delay time.Duration) *PublishedMessage {
+	if data := s.Next(); data != nil {
+		return data
+	}
+
+	<-time.After(delay)
+	return s.Next()
 }
 
 func (s *Subscriber) AddSubscription(pubsub *PubSub, topics ...string) (bool, uint64) {
