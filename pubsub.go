f1322f19bfc41c988bcb4effa0f7559020121833
Anjan Roy <anjanroy@yandex.com>
2021-04-27 07:49:36 +0530

    new pubsub system

--- pubsub.go
+++ pubsub.go
@@ -9,3 +9,10 @@ type PubSub struct {
 	Message     <-chan Message
 	Subscribers map[string][]*Subscriber
 }
+
+func New() *PubSub {
+	return &PubSub{
+		Message:     make(<-chan Message, 256),
+		Subscribers: make(map[string][]*Subscriber),
+	}
+}
