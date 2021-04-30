1dee56e278378eccc6b38a38e8981e5fc11b2035
Anjan Roy <anjanroy@yandex.com>
2021-04-30 17:36:18 +0530

    not plotting for spsc

--- stress/spsc/main.go
+++ stress/spsc/main.go
@@ -4,12 +4,9 @@ import (
 	"context"
 	"log"
 	"math/rand"
-	"os"
 	"time"
 
 	"github.com/c2h5oh/datasize"
-	"github.com/go-echarts/go-echarts/v2/charts"
-	"github.com/go-echarts/go-echarts/v2/opts"
 	"github.com/itzmeanjan/pubsub"
 )
 
@@ -23,7 +20,7 @@ func getRandomByteSlice(len int) []byte {
 	return buffer
 }
 
-func simulate(target uint64, cap uint64) (bool, time.Duration) {
+func simulate(target uint64) (bool, time.Duration) {
 
 	broker := pubsub.New()
 
@@ -33,7 +30,7 @@ func simulate(target uint64, cap uint64) (bool, time.Duration) {
 
 	<-time.After(time.Duration(100) * time.Microsecond)
 
-	subscriber := broker.Subscribe(cap, "topic_1")
+	subscriber := broker.Subscribe(target, "topic_1")
 	if subscriber == nil {
 		log.Printf("Failed to subscribe\n")
 		return false, 0
@@ -80,6 +77,7 @@ func simulate(target uint64, cap uint64) (bool, time.Duration) {
 		if done == target {
 			break
 		}
+
 	}
 
 	<-signal
@@ -87,46 +85,13 @@ func simulate(target uint64, cap uint64) (bool, time.Duration) {
 
 }
 
-func drawChart(xAxis []string, durations []opts.BarData, sink string) {
-
-	bar := charts.NewBar()
-
-	bar.SetGlobalOptions(
-		charts.WithTitleOpts(opts.Title{
-			Title: "Single Producer Single Consumer",
-		}),
-		charts.WithLegendOpts(opts.Legend{Right: "80%"}),
-		charts.WithXAxisOpts(opts.XAxis{
-			Name: "Data",
-		}),
-		charts.WithYAxisOpts(opts.YAxis{
-			Name:      "Time",
-			AxisLabel: &opts.AxisLabel{Show: true, Formatter: "{value} ms"},
-		}))
-
-	bar.SetXAxis(xAxis).
-		AddSeries("Duration", durations).
-		SetSeriesOptions(charts.WithLabelOpts(opts.Label{
-			Show:      true,
-			Position:  "top",
-			Formatter: "{c}",
-		}))
-
-	f, _ := os.Create(sink)
-	bar.Render(f)
-
-}
-
 func main() {
-
-	var xAxis = make([]string, 0)
-	var durations = make([]opts.BarData, 0)
 
 	for i := 1; i <= 1024; i *= 2 {
 
 		target := uint64(i * 1024)
 
-		ok, timeTaken := simulate(target, 1024)
+		ok, timeTaken := simulate(target)
 		if !ok {
 			log.Printf("❌ %s\n", datasize.KB*datasize.ByteSize(target))
 			continue
@@ -134,14 +99,6 @@ func main() {
 
 		log.Printf("✅ %s in %s\n", datasize.KB*datasize.ByteSize(target), timeTaken)
 
-		xAxis = append(xAxis, (datasize.KB * datasize.ByteSize(target)).String())
-		durations = append(durations, opts.BarData{
-			Value: timeTaken / (time.Duration(1) * time.Millisecond),
-		})
-
 	}
-
-	drawChart(xAxis, durations, "../charts/spsc.html")
-	log.Printf("Plotted bar chart for SPSC\n")
 
 }
